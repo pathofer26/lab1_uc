12b8bfec989ebba0da95cad10687426f44b70b50
FERNANDO.G <luisfernando.granados@uptc.edu.co>
2017-03-16 13:00:32 -0500

    reset rutina

--- build/default/production/lab_uno.o.d
+++ build/default/production/lab_uno.o.d
@@ -2,5 +2,5 @@ build/default/production/lab_uno.o:  \
 	D:/Pc/UPTC/I\ SEM-2017/MICROC-2017/LABORATORIO/lab1/lab_uno.X/lab_uno.asm  \
 	D:/Pc/UPTC/I\ SEM-2017/MICROC-2017/LABORATORIO/lab1/lab_uno.X/p16f887.inc  \
 	D:/Pc/UPTC/I\ SEM-2017/MICROC-2017/LABORATORIO/lab1/lab_uno.X/config.inc  \
-	D:/Pc/UPTC/I\ SEM-2017/MICROC-2017/LABORATORIO/lab1/lab_uno.X/Retardos.inc  \
+	D:/Pc/UPTC/I\ SEM-2017/MICROC-2017/LABORATORIO/lab1/lab_uno.X/RETARDOS.inc  \
 
--- lab_uno.asm
+++ lab_uno.asm
@@ -1,14 +1,17 @@
-;----------------------------- ENCABEZADO --------------------------------------
-    list p=16f887           
+;******************************************************************************************************
+list p=16F887
+#include"p16f887.inc"
+;--------------------------- CONFIGURACION DE PUERTOS  -------------------------
     #include "p16f887.inc"
 
-    ; CONFIG1
-    ; __config 0xE3F4
-     __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
-    ; CONFIG2
-    ; __config 0xFEFF
-     __CONFIG _CONFIG2, _BOR4V_BOR21V & _WRT_OFF
-;-------------------------------------------------------------------------------    
+; CONFIG1
+; __config 0xE0F5
+ __CONFIG _CONFIG1, _FOSC_INTRC_CLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFFFF
+ __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
+ 
+;********************************************************
  CBLOCK 0X20
  REG1
  REG2
@@ -22,6 +25,9 @@
  REGH
  REG1AUX
  CUENTA1
+ K
+ M
+ N
 
  ENDC
  
@@ -29,11 +35,13 @@
  ORG	.0
  
 INICIO:
- call	configuracion
+ CALL    OSC_CONF
+ CALL    CONFIGURACION
  CLRF	PORTB	    ;CLAREAR PUERTOS A Y B
  CLRF	PORTC	    ;
 
 PRINCIPAL:
+	    CLRF    REGH1
 	    MOVF    PORTA,w
 	    MOVWF   REGH1
 	 CONTEO
@@ -47,7 +55,7 @@ PRINCIPAL:
 	    MOVWF   PORTB
 	    MOVF    CENTENA,w
 	    MOVWF   PORTC 
-	    CALL    Retardo_500ms
+	    CALL    RETARDO_500ms
 	    MOVLW   0XFF
             SUBWF   REGH1,w
 	    BTFSC   STATUS,Z
@@ -56,7 +64,7 @@ PRINCIPAL:
 	    MOVF    REGH1,W
 	    BTFSS   PORTD,RD0
 	    GOTO    CONTEO
-	    CALL    Retardo_2ms
+	    CALL    RETARDO_250ms
 	    GOTO    PRINCIPAL
 	    
 	    
@@ -66,40 +74,51 @@ PRINCIPAL:
 	    CLRF    PORTB      
 	    MOVLW   .8 
 	    MOVWF   CUENTA1 
-BUCLE1	    BSF	    STATUS,C 
+BUCLE1	    BTFSC   PORTD,RD0
+	    GOTO    X
+	    BSF	    STATUS,C 
 	    RRF	    PORTC,1  ;RRF
-	    CALL    Retardo_500ms   
+	    CALL    RETARDO_500ms   
 	    DECFSZ  CUENTA1,1 
 	    GOTO    BUCLE1 
 	    MOVLW   .8 
 	    MOVWF   CUENTA1  
-BUCLE2	    BSF STATUS,C 
+BUCLE2	    BTFSC   PORTD,RD0
+	    GOTO    X
+	    BSF STATUS,C 
 	    RRF PORTB,1  ;RRF
-	    CALL    Retardo_500ms 
+	    CALL    RETARDO_500ms 
 	    DECFSZ  CUENTA1,1 
 	    GOTO BUCLE2 
 	    MOVLW .8 
 	    MOVWF   CUENTA1 
-BUCLE3	    BCF STATUS,C 
+BUCLE3	    BTFSC   PORTD,RD0
+	    GOTO    X
+	    BCF STATUS,C 
 	    RLF PORTB,1  ;RLF
-	    CALL    Retardo_500ms 
+	    CALL    RETARDO_500ms 
 	    DECFSZ  CUENTA1,1 
 	    GOTO BUCLE3
 	    MOVLW .4 
 	    MOVWF   CUENTA1 
-BUCLE4	    BCF STATUS,C 
+BUCLE4	    BTFSC   PORTD,RD0
+	    GOTO    X
+	    BCF STATUS,C 
 	    RLF PORTC,1  ;RLF
-	    CALL    Retardo_500ms 
+	    CALL    RETARDO_500ms 
 	    DECFSZ  CUENTA1,1 
 	    GOTO BUCLE4
 	    MOVLW   .0
 	    MOVWF   REG1
 	    RETURN
-;****************************************************************	    
+;****************************************************************
+    X       CALL    RETARDO_250ms
+	    GOTO    PRINCIPAL
+;**********
 
 
 #include    "config.inc"
-#include    "Retardos.inc"
+#include    "RETARDOS.inc"
 END
      
      
